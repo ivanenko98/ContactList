d4d2088819dd981c0723f8e3aed53e9a342d7450
Ivanenko Oleg <ivanenko.oleg.m@gmail.com>
2017-08-22 12:19:06 +0300

    add new contact

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ function viewListContacts() {
     if (returnContacts !== null){
         returnContacts.forEach(viewArrayElements);
     }else{
-        console.log('add  contact');
+        console.log('add new contact');
     }
 
 }
